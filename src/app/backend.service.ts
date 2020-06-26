89580c6311bdbacdb9b10f3aa685d9aec3122f4f
Nishu2696 <nishaanth2696@gmail.com>
2020-06-26 13:34:31 +0530

    service update

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -8,50 +8,51 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class BackendService {
+  link = "https://urlshortner-nishu.herokuapp.com/";
 
   constructor(
     private http: HttpClient
   ) { }
 
   register(reg_details): Observable<any>{
-    return this.http.post(`${environment.apiURL}register`, reg_details);
+    return this.http.post( `${this.link}register`, reg_details);
     //return this.http.post("http://localhost:3000/register", reg_details);
   }
 
   login(log_details): Observable<any>{
-    return this.http.post(`${environment.apiURL}login`, log_details);
+    return this.http.post(`${this.link}login`, log_details);
     //return this.http.post("http://localhost:3000/login", log_details);
   }
 
   verifying_email(verify_details): Observable<any>{
-    return this.http.post(`${environment.apiURL}verificationemail`, verify_details);
+    return this.http.post(`${this.link}verificationemail`, verify_details);
     return this.http.post("http://localhost:3000/verificationemail", verify_details);
   }
 
   shorturl(url_details): Observable<any>{
-    return this.http.post(`${environment.apiURL}createurlshortner`, url_details);
+    return this.http.post(`${this.link}createurlshortner`, url_details);
     //return this.http.post("http://localhost:3000/createurlshortner", url_details);
   }
 
   
 
   reset(reset_details): Observable<any>{
-    return this.http.post(`${environment.apiURL}`, reset_details);
+    return this.http.post(`${this.link}changepassword`, reset_details);
     //return this.http.post("http://localhost:3000/changepassword", reset_details)
   }
 
   change(change_details): Observable<any>{
-    return this.http.post(`${environment.apiURL}interchangepassword`, change_details);
+    return this.http.post(`${this.link}interchangepassword`, change_details);
     //return this.http.post("http://localhost:3000/interchangepassword", change_details)
   }
 
   getdata(): Observable<any>{
-    return this.http.get(`${environment.apiURL}urlshortnerapi`);
+    return this.http.get(`${this.link}urlshortnerapi`);
     // return this.http.get("http://localhost:3000/urlshortnerapi");
   }
 
   getcount(value): Observable<any>{
-    return this.http.get(`${environment.apiURL}+${value}`);
+    return this.http.get(`${this.link}+${value}`);
     //return this.http.get(`http://localhost:3000/${value}`);
   }
 }
